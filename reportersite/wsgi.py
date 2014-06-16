dee2bb43f7912d061e8682e78e5ac9fbc88fc728
Yiwen Zhan <theyiwen@gmail.com>
2014-06-15 22:07:29 -0700

    wsgi change

--- reportersite/wsgi.py
+++ reportersite/wsgi.py
@@ -11,4 +11,8 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reportersite.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
